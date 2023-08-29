ecbca1a8235f82c73a18abd266314c0cbb2f27ab
Serious-Fin <augustasgri@gmail.com>
2023-08-29 22:36:09 +0300

    add input field to main page for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Timer from "./components/Timer";
+import TimeInput from "./components/TimeInput";
 
 function App() {
   return (
     <>
       <div>
         <h1>Main App</h1>
+        <hr />
+        <TimeInput />
+        <hr />
         <Timer initialTime={5} />
       </div>
     </>
